51e9a83fd3256dd7ff86ddee7b8bfab1b2df558a
Shmorgun Eugene <eshmorgun@amtoss.com.ua>
2021-04-17 21:30:49 +0300

    web version of apps added

--- starter/src/main/java/com/example/starter/MainVerticle.java
+++ starter/src/main/java/com/example/starter/MainVerticle.java
@@ -2,22 +2,28 @@ package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+  public void start() throws Exception {
+    HttpServer server = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+    router.route().handler(ctx -> {
+
+      // This handler will be called for every request
+      HttpServerResponse response = ctx.response();
+      response.putHeader("content-type", "text/plain");
+
+      // Write to the response and end it
+      response.end("Hello World from Vert.x-Web 1!");
     });
+
+    server.requestHandler(router).listen(8080);
   }
 }
